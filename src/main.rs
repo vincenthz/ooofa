2f6539c35df2743a725785d33075967a440e5e13
Vincent Hanquez <vincent@typed.io>
2021-09-01 14:15:04 +0800

    improvement to display

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use chrono::offset::Local;
+use chrono::DateTime;
+use console::Style;
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use serde::Deserialize;
 use serde_yaml;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::io::Read;
 use std::path::PathBuf;
 
 #[derive(Debug, Clone, Deserialize)]
 struct Config {
-    keys: HashMap<String, String>,
+    keys: BTreeMap<String, String>,
 }
 
 fn print_left(d: std::time::Duration) -> String {
@@ -16,6 +19,11 @@ fn print_left(d: std::time::Duration) -> String {
     let x = millis / 10;
 
     format!("{:02}.{:02} seconds", secs, x)
+}
+
+fn print_time(time: std::time::SystemTime) -> String {
+    let datetime: DateTime<Local> = time.into();
+    format!("{}", datetime.format("%H:%M:%S"))
 }
 
 fn main() {
@@ -30,7 +38,21 @@ fn main() {
     let mut file = std::fs::File::open(config).unwrap();
     file.read_to_string(&mut contents).unwrap();
 
+    // read the config
     let config: Config = serde_yaml::from_str(&contents).unwrap();
+
+    // from the config, create all the keys
+    let keys: BTreeMap<_, _> = config
+        .keys
+        .iter()
+        .map(|(k, v)| {
+            let otp = aotp::OTP::from_url(
+                &url::Url::parse(&v).expect(&format!("key for '{}' is not a valid url", k)),
+            )
+            .expect(&format!("key for '{}' is not a valid otp construction", k));
+            (k, otp)
+        })
+        .collect();
 
     let keyt = &args[1];
     if keyt == "watch" {
@@ -39,6 +61,88 @@ fn main() {
             .template("{spinner} {prefix:.bold.dim} {wide_msg}");
 
         let m = MultiProgress::new();
+
+        // sort the keys per type of period
+        // and create multiple progress bar if necessary:
+        // one per period category
+        // and one per keys in those periods
+        let periods = [aotp::Period::seconds30()];
+        let mut keys_per_period = BTreeMap::new();
+        for period in periods {
+            let mut out = BTreeMap::new();
+            for (name, key) in keys.iter() {
+                if key.period != period {
+                    continue;
+                }
+                out.insert(name, key.clone());
+            }
+            if !out.is_empty() {
+                let period_progress_bar = m.add(ProgressBar::new(1));
+                period_progress_bar.set_style(spinner_style.clone());
+                period_progress_bar.set_prefix(format!("{:?}  : ", period));
+                let mut key_entries = Vec::new();
+                for (name, k) in out.into_iter() {
+                    let key_bar = m.add(ProgressBar::new(1));
+                    key_bar.set_style(spinner_style.clone());
+                    key_bar.set_prefix(format!("{:10}  : ", name));
+                    key_entries.push((k, key_bar))
+                }
+                keys_per_period.insert(period, (period_progress_bar, key_entries));
+            }
+        }
+
+        std::thread::spawn(move || loop {
+            let cyan = Style::new().cyan();
+            let green = Style::new().green();
+            let red = Style::new().red();
+
+            let threshold = std::time::Duration::new(5, 0);
+
+            let mut previous_counter = aotp::Counter::zero();
+            let mut threshold_change = false;
+            let sleep = std::time::Duration::from_millis(220);
+            for (period, (period_bar, keys)) in keys_per_period.iter() {
+                let (counter, left) = aotp::Counter::totp_now_left((*period).into());
+                let next_counter = counter.incr();
+                let current_start = counter.system_time((*period).into());
+                let next_start = next_counter.system_time((*period).into());
+
+                // set the color for looming stuff
+                let looming = left <= threshold;
+                let color = if looming { &red } else { &cyan };
+
+                let threshold_trigger = threshold_change ^ looming;
+                threshold_change = looming;
+
+                period_bar.set_message(format!(
+                    "current-period: {:?} -- started-at: {} -- left: {} -- next-at: {}",
+                    counter,
+                    green.apply_to(print_time(current_start)),
+                    color.apply_to(print_left(left)),
+                    green.apply_to(print_time(next_start)),
+                ));
+
+                // check if the counter has changed, if it has not, then we just update the period bar
+                if counter == previous_counter && !threshold_trigger {
+                    //
+                } else {
+                    for (otp, key_bar) in keys.iter() {
+                        let token = otp.totp_at(counter);
+                        let token_next = otp.totp_at(next_counter);
+                        key_bar.set_message(format!(
+                            " current: {}      | next: {}",
+                            color.apply_to(token.dec6()),
+                            //print_left(left),
+                            green.apply_to(token_next.dec6())
+                        ));
+                    }
+                    previous_counter = counter;
+                }
+            }
+            std::thread::sleep(sleep);
+        });
+
+        /*
         for (k, v) in config.keys {
             let pb = m.add(ProgressBar::new(1));
             pb.set_style(spinner_style.clone());
@@ -61,6 +165,7 @@ fn main() {
                 std::thread::sleep(sleep);
             });
         }
+        */
         m.join().unwrap();
     } else {
         let x = config.keys.keys().find(|k| k.starts_with(keyt));
